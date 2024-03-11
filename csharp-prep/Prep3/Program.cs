7e9b139e5d791993dc844ff4033142a611c48ba9
lily <lilypromise99@gmail.com>
2024-03-11 01:23:44 -0200

    c# project

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,34 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        // generating a random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            //// user specified the number
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
